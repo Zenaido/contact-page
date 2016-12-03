2161cfd6435ae748ff5112ff341a294c0cdef633
Zenaido <luis.hernandez@aggiemail.usu.edu>
2016-12-03 00:25:39 -0700

    all files

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://lzhernandez96@gmail.com:izqjmdwegupqmvjd@smtp.gmail.com');
+
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index');
+
+});
+router.post('/',function (req,res,next) {
+  var name = req.body.fName;
+  var lname = req.body.lName;
+  var email = req.body.email;
+
+  var mailOptions = {
+    from: '"Web Contact Page" <' + email +'>', // sender address
+    to: '<lzhernandez96@gmail.com>', // list of receivers
+    subject: 'Contact Form', // Subject line
+    //text: 'Hello world 🐴', // plaintext body gmail accepts html so I won't bother
+    html: '<b>'+ name  +' '+ lname + '</b>' +
+    '<b>' + email+ '</b>' // html body
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+
+
+  res.render('index');
+
+
+})
+
+module.exports = router;
